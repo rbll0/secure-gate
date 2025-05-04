d2a88f7ce144b7c3952e288b827d193dbf8409b2
Gustavo Rabelo Frere <gurabelo09@gmail.com>
2025-05-04 19:08:31 -0300

    init project

---
+++ src/main/java/com/securegate/securegate/SecureGateApplication.java
@@ -0,0 +1,13 @@
+package com.securegate.securegate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureGateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecureGateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/securegate/securegate/SecureGateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.securegate.securegate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureGateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
